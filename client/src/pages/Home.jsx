4e7d19fe181ded751af0dab8a37ca116504cf45d
Ritesh Pandey <riteshpandey72060@gmail.com>
2024-03-07 19:27:12 +0530

    commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-purple-800'>
+      <div className='text-white font-bold text-10xl animate__animated animate__fadeInUp'>
+        BLAZEKARO
+      </div>
+    </div>
+  );
+}
+
+export default Home;

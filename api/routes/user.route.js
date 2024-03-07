b70a35003b74286621fb09b3b262bcb3fd764674
Ritesh Pandey <riteshpandey72060@gmail.com>
2024-03-07 13:30:15 +0530

    commit

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,11 @@
 import {Router} from "express"
 import  {Signin,Signup, logoutUser, Home} from "../controllers/user.controller.js"
-import {verifyJWT} from "../middleware/authmiddleware.js"
 const router = Router();
 
 router.route("/signup").post(Signup);
 router.route("/signin").post(Signin);
-router.route("/logout").post(verifyJWT,  logoutUser)
-router.route("/home").get(verifyJWT,Home);
+router.route("/logout").post( logoutUser)
+router.route("/home").get(Home);
 
 
 export default router;
